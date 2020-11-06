0208b0a16df76d834f0f4439cf015099456c323c
Yu-suke441 <it.creative.pgm.738@gmail.com>
2020-11-06 21:24:23 +0900

    categoryを追加。categoryと合致するTaskのみを絞り込み表示する機能を実装。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,20 +7,39 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
-    
-
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchBarField: UISearchBar!
+    // Realmインスタンスを取得する
+    let realm = try! Realm()
+    
+    // DB内のタスクが格納されるリスト
+    // 日付の近い順でソート:昇順
+    // 以降内容をアップデートするとリスト内は自動的に更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBarField.delegate = self
+        searchBarField.showsCancelButton = true
+        searchBarField.enablesReturnKeyAutomatically = false
+        searchBarField.enablesReturnKeyAutomatically = true
     }
 
     // データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
     
     // 各セルの内容を返すメソッド
@@ -28,7 +47,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // 再利用可能なCellを得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        // Cellに値を設定する
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.endEditing(true)
+        
+        guard let searchText = searchBar.text else {return}
+        
+        
+        let result = realm.objects(Task.self).filter("category BEGINSWITH  '\(searchText)'")
+        
+        
+        
+        let count = result.count
+        
+        if(count == 0) {
+            taskArray = realm.objects(Task.self)
+        } else {
+            taskArray = result
+        }
+        tableView.reloadData()
     }
     
     // 各セルを選択した時に実行されるメソッド
@@ -43,7 +93,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //　Delete ボタンが押された時によばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
+    
+    // segueで画面遷移する時によばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let inputVC:InputViewController = segue .destination as! InputViewController
         
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputVC.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputVC.task = task
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 }
 
